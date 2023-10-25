705aaa03aa3445f6e247f2cb0faf259896f958da
Jessica Biondi <jbiondi.cs@gmail.com>
2023-10-25 17:49:00 -0400

    added documentation and tests for sqlForPartialUpdate function

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,7 +2,20 @@
 
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Takes in two objects,
+ * dataToUpdate: an object which is the parsed JSON from the request body representing
+ * the information that the client wants to update
+ * {numEmployees: 333, logoUrl: "https://myPhoto.com"}
+ *
+ * and jsToSql: an object where the values represent the snake case names that we
+ * want to convert the column names to for the SQL query
+ * {numEmployees: "num_employees", logoUrl: "logo_url", ...}
+ *
+ * Returns an object which will be used in data model update methods for SQL queries
+ * which now has correctly cased column names
+ * {setCols: '"num_employees"=$1, "logo_url"=$2', values: [333, "https://myPhoto.com"]}
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
---
+++ helpers/sql.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { BadRequestError } = require("../expressError");
+const { sqlForPartialUpdate } = require("./sql");
+
+describe("testing sqlForPartialUpdate", function () {
+
+  test("valid output given correct input", function () {
+    const result = sqlForPartialUpdate(
+      { numEmployees: 333, logoUrl: "https://myPhoto.com" },
+      { numEmployees: "num_employees", logoUrl: "logo_url" }
+    );
+    expect(result).toEqual(
+      {
+        setCols: '"num_employees"=$1, "logo_url"=$2',
+        values: [333, "https://myPhoto.com"]
+      }
+    );
+  });
+
+
+  test("test error if no dataToUpdate keys", function () {
+    // why does this need to wrapped in another function?
+    expect(function () {
+      sqlForPartialUpdate(
+        {},
+        { numEmployees: "num_employees", logoUrl: "logo_url" }
+      );
+    }).toThrow(BadRequestError);
+  });
+
+
+});
+
+
